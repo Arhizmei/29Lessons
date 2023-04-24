5008dbcf641521ad4c2ca5c0572c2998a630ef85
Arhizmei <arhizmei@gmail.com>
2023-04-24 23:11:45 +0300

    home

--- app/src/main/java/com/zmei/a29lessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a29lessons/MainActivity.kt
@@ -2,10 +2,32 @@ package com.zmei.a29lessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.zmei.a29lessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.bottomMenu.selectedItemId = R.id.item3
+        binding.bottomMenu.setOnNavigationItemReselectedListener {
+            when (it.itemId){
+                R.id.item1 ->{
+                    Toast.makeText(this, "Item1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 ->{
+                    Toast.makeText(this, "Item2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 ->{
+                    Toast.makeText(this, "Item3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 ->{
+                    Toast.makeText(this, "Item4", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+            true
     }
-}
+}}
